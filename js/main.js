9962dc7884154338d89bdd34758b33bebf60d5b4
TheKolmik <namexin@seznam.cz>
2022-02-26 05:24:44 +0100

    twoNewFunctions

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function getRangeMM(min, max) {
+  if (min >= 0) {
+    if(max <= min) {
+      max = min;
+    }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } else {
+    return 'ERROR: minimum value must be 0';
+  }
+}
+getRangeMM(0,100);
+
+function getRangeMMD(min,max,digits) {
+  if (min >= 0) {
+    if(max <= min) {
+      max = min;
+    }
+    let number = (Math.random() * (max - min + 1)) + min;
+    return number.toFixed(digits);
+  } else {
+    return 'ERROR: minimum value must be 0';
+  }
+
+
+}
+getRangeMMD(0,10099,10);
